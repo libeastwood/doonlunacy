3793641e808167216c1d782f939314741040efd6
Per Øyvind Karlsen <peroyvind@localhost>
2008-08-04 23:35:31 +0200

    add a class inheriting Image class for drawing more specific stuff

---
+++ include/DrawImage.h
@@ -0,0 +1,35 @@
+#ifndef DUNE_DRAWIMAGE_H
+#define DUNE_DRAWIMAGE_H
+
+#include "Gfx.h"
+#include "Definitions.h"
+
+class DrawImage : public Image
+{
+    public:
+        DrawImage(ConstUPoint size, Uint32 color = -1);
+        DrawImage(Image *image);
+	~DrawImage();
+
+        void drawBorders(GuiPic_enum nw, GuiPic_enum ne, GuiPic_enum sw,
+			GuiPic_enum se, Uint16 edgeDistance = 0);
+        void drawBorders1(Uint16 edgeDistance = 0)
+	{
+		drawBorders(UI_Corner1NW, UI_Corner1NE, UI_Corner1SW,
+				UI_Corner1SE, edgeDistance);
+	}
+
+        void drawBorders2(Uint16 edgeDistance = 0)
+	{
+		drawBorders(UI_Corner2NW, UI_Corner2NE, UI_Corner2SW,
+				UI_Corner2SE, edgeDistance);
+	}
+
+        void drawBorders3(Uint16 edgeDistance = 0)
+	{
+		drawBorders(UI_Corner3NW, UI_Corner3NE, UI_Corner3SW,
+				UI_Corner3SE, edgeDistance);
+	}
+
+};
+#endif // DUNE_DRAWIMAGE_H
---
+++ src/DrawImage.cpp
@@ -0,0 +1,50 @@
+#include "DataCache.h"
+#include "DrawImage.h"
+
+DrawImage::DrawImage(ConstUPoint size, Uint32 color) : Image(size)
+{
+	if(color != (Uint32)-1)
+		fillRect(color, Rect(0, 0, size.x, size.y));
+}
+
+DrawImage::DrawImage(Image *image) : Image(image->getSurface())
+{
+}
+
+DrawImage::~DrawImage()
+{
+}
+
+
+void DrawImage::drawBorders(GuiPic_enum nw, GuiPic_enum ne, GuiPic_enum sw,
+		GuiPic_enum se, Uint16 edgeDistance)
+{
+    ImagePtr corner_nw, corner_ne, corner_sw, corner_se, top, bottom, left, right;
+    UPoint size = getSize();
+
+    corner_nw = DataCache::Instance()->getGuiPic(nw);
+    blitFrom(corner_nw.get(), UPoint(edgeDistance,edgeDistance));
+    corner_ne = DataCache::Instance()->getGuiPic(ne);
+    blitFrom(corner_ne.get(), UPoint(size.x - corner_ne->getSize().x - edgeDistance, edgeDistance));
+    corner_sw = DataCache::Instance()->getGuiPic(sw);
+    blitFrom(corner_sw.get(), UPoint(edgeDistance, size.y - corner_sw->getSize().y - edgeDistance));
+    corner_se = DataCache::Instance()->getGuiPic(se);
+    blitFrom(corner_se.get(), UPoint(size.x - corner_se->getSize().x - edgeDistance, size.y - corner_se->getSize().y - edgeDistance));
+
+    top = DataCache::Instance()->getGuiPic(UI_TopBorder);
+    bottom = DataCache::Instance()->getGuiPic(UI_BottomBorder);
+
+    for(int i = corner_se->getSize().x; i < size.x - corner_se->getSize().y; i++){
+        blitFrom(top.get(), UPoint(i, edgeDistance));
+        blitFrom(top.get(), UPoint(i, size.y - bottom->getSize().y - edgeDistance));
+    }
+
+    left = DataCache::Instance()->getGuiPic(UI_LeftBorder);
+    right = DataCache::Instance()->getGuiPic(UI_RightBorder);
+    for(int i = corner_ne->getSize().y; i < size.y - corner_se->getSize().x; i++){
+        blitFrom(left.get(), UPoint(edgeDistance, i));
+        blitFrom(right.get(), UPoint(size.x - right->getSize().x - edgeDistance, i));
+    }
+
+}
+
