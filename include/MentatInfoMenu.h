2b024ed96d11d8c3cf7744ca8b91183c59fd2397
Per Øyvind Karlsen <peroyvind@localhost>
2008-08-04 02:44:37 +0200

    forgot to add and commit these in previous commit..

---
+++ include/MentatInfoMenu.h
@@ -0,0 +1,21 @@
+#ifndef MENTAITNFOMENU_H_INCLUDED
+#define MENTATINFOMENU_H_INCLUDED
+
+#include "MentatMenu.h"
+class GraphicButton;
+class AnimationLabel;
+class MentatInfoMenuState : public MentatMenuState {
+public:
+	MentatInfoMenuState(HOUSETYPE newHouse);
+	~MentatInfoMenuState();
+	
+	virtual const char* GetName() { return "MentatInfoMenuState"; }
+		
+private:
+        GraphicButton* m_butExit;
+	AnimationLabel* m_planetAnimation;
+	void doExit();
+};
+
+#endif // MENTATINFOMENU_H_INCLUDED
+
---
+++ src/MentatInfoMenu.cpp
@@ -0,0 +1,32 @@
+#include <boost/bind.hpp>
+#include "MentatInfoMenu.h"
+
+#include "Application.h"
+#include "Settings.h"
+#include "DataCache.h"
+#include "Animation.h"
+#include "gui2/Label.h"
+#include "gui2/Button.h"
+#include <StringFile.h>
+#include <string>
+
+MentatInfoMenuState::MentatInfoMenuState(HOUSETYPE newHouse) : MentatMenuState(newHouse)
+{
+	m_butExit = new GraphicButton(DataCache::Instance()->getGuiPic(UI_MentatExit)->getResized(), DataCache::Instance()->getGuiPic(UI_MentatExit_Pressed)->getResized());
+	m_butExit->setPosition(sizeRelativeUPoint(200, 168));
+	m_butExit->onClick.connect(
+        boost::bind(&MentatInfoMenuState::doExit, this) );    
+	    m_container->addChild(m_butExit);
+
+	drawSpecificStuff();
+}
+
+MentatInfoMenuState::~MentatInfoMenuState()
+{
+}
+
+void MentatInfoMenuState::doExit()
+{
+    assert(mp_parent != NULL);
+    PopState();
+}
