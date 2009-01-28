e819b52a692cb527bc8b1862bf187ed2929dd8c3
Per Øyvind Karlsen <proyvind@gmail.com>
2009-01-28 21:18:44 +0100

    move Point & Rect structs into their own headers so that they can be used by classes without having to include the whole Gfx class (which might lead to conflicts as well)

---
+++ include/Point.h
@@ -0,0 +1,198 @@
+#ifndef DUNE_POINT_H
+#define DUNE_POINT_H
+
+#include "Settings.h"
+
+#include <iostream>
+
+//------------------------------------------------------------------------------
+// Point struct
+//------------------------------------------------------------------------------
+//! The Point struct is basic representation of a point
+/*!
+    @note Implemented fully inline, so no need to worry about speed.
+*/
+template <typename T>
+struct Point
+{
+    public:
+        //! @name Member variables
+        //@{
+
+        //! x-coordinate
+        T x;
+        //! y-coordinate
+        T y;
+
+        //@}
+        
+
+        //! @name Constructors & Destructor
+        //@{
+
+        //! Constructor
+        Point(T x = 0, T y = 0) : x(x), y(y) {  }
+        //! Copy constructor
+        Point(const Point &point) { *this = point; }
+        
+        //@}
+
+        //! @name Operators
+        //@{
+        
+        //! operator =
+        Point &operator=(const Point &point)
+        {
+            x = point.x;
+            y = point.y;
+            return *this;
+        }
+        //! operator +=
+        Point &operator+=(const Point &point)
+        {
+            x += point.x;
+            y += point.y;
+            return *this;
+        }
+        //! operator -=
+        Point &operator-=(const Point &point)
+        {
+            x += point.x;
+            y += point.y;
+            return *this;
+        }
+        //! operator *=
+        Point &operator*=(T times)
+        {
+            x *= times;
+            y *= times;
+            return *this;
+        }
+        //! operator /=
+        /*
+            @warning: Do not divide by zero !
+        */
+        Point &operator/=(T times)
+        {
+            // TODO: assert ?
+            x /= times;
+            y /= times;
+            return *this;
+        }
+        
+        //! operator +
+        Point operator+(const Point &point) const
+        {
+            return Point(x + point.x, y + point.y);
+        }
+        //! operator -
+        Point operator-(const Point &point) const
+        {
+            return Point(x - point.x, y - point.y);
+        }
+        //! operator *
+        Point operator*(T times) const
+        {
+            return Point(x*times, y*times);
+        }
+        //! operator /
+        /*!
+            @warning: Do not divide by zero !
+        */
+        Point operator/(T times) const
+        {
+            // TODO: assert ?
+            return Point(x/times, y/times);
+        }
+        
+        friend bool operator==(const Point& a, const Point& b){
+            return (a.x == b.x && a.y == b.y);
+        }
+
+        friend bool operator!=(const Point& a, const Point& b){
+            return (a.x != b.x || a.y != b.y);
+        }
+
+		friend std::ostream& operator<<(std::ostream& os, const Point& c){
+			return os << '(' << c.x << 'x' << c.y << ')';
+		}
+
+        //! Type-casting operator
+        /*!
+            If it doesn't compile, then the types are incompatible.
+        */
+        template <typename T2>
+        operator Point<T2>() const
+        {
+            return Point<T2>(x, y);
+        }
+
+        //@}
+
+        //! @name Miscellaneous methods
+        //@{
+        
+        //! Swap x<->y coordinates
+        const Point &swap()
+        {
+            T t = x;
+            x = y;
+            y = t;
+        }
+        
+        //! Return point with swapped x<->y coordinates
+        Point getSwapped() const
+        {
+            return Point(y, x);
+        }
+
+	//! Return point scaled to current resolution
+        Point getScaled() const
+        {
+            return Point((Settings::Instance()->GetWidth()  / 320.0f) * x,
+                         (Settings::Instance()->GetHeight() / 200.0f) * y);
+        }
+             
+        //@}
+};
+
+//! Point with unsigned coordinates
+typedef Point<Uint16> UPoint;
+//! Constant Point with unsigned coordinates
+typedef const UPoint &ConstUPoint;
+
+//! Point with signed coordinates
+typedef Point<Sint16> SPoint;
+//! Constant Point with signed coordinates
+typedef const SPoint &ConstSPoint;
+
+//! Point with signed 32-bit coordinates
+typedef Point<Sint32> SPointBig;
+//! Constant Point with signed 32-bit coordinates
+typedef const SPointBig &ConstSPointBig;
+
+//! Point with float coordinates
+typedef Point<float>  PointFloat;
+//! Constant Point with float coordinates
+typedef const PointFloat &ConstPointFloat;
+
+//! @name Unary minus operators for signed points
+//! @relates Point
+//@{
+inline SPoint operator-(const SPoint &point)
+{
+    return SPoint(point.x, point.y);
+}
+
+inline SPointBig operator-(const SPointBig &point)
+{
+    return SPointBig(point.x, point.y);
+}
+
+inline PointFloat operator-(const PointFloat &point)
+{
+    return PointFloat(point.x, point.y);
+}
+//@}
+
+#endif // DUNE_POINT_H
---
+++ include/Rect.h
@@ -0,0 +1,155 @@
+#ifndef DUNE_RECT_H
+#define DUNE_RECT_H
+
+#include <SDL.h>
+
+//------------------------------------------------------------------------------
+// Rect struct
+//------------------------------------------------------------------------------
+//! The Rect struct is an extension of SDL_Rect
+/*!
+    This struct provides convenient constructors and methods to test if Rect
+    contains another Rect or Point.
+    
+    @note Member wariables are x,y (Sint16) and w,h (Uint16) inherited from 
+    SDL_Rect
+    
+    @note Implemented fully inline, so no need to worry about speed.
+*/
+struct Rect : public SDL_Rect
+{
+    public:
+ 
+        //! @name Constructors & Destructor
+        //@{
+
+        //! Constructor
+        Rect(Sint16 x = 0, Sint16 y = 0, Uint16 w = 0, Uint16 h = 0)
+        {
+            this->x = x;
+            this->y = y;
+            this->w = w;
+            this->h = h;
+        }
+        //! Constructor
+        Rect(const SPoint &position, const UPoint &size)
+        {
+            setPosition(position);
+            setSize(size);
+        }
+        //! Copy constructor
+        Rect(const Rect &rect)
+        {
+            x = rect.x;
+            y = rect.y;
+            w = rect.w;
+            h = rect.h;
+        }
+        //! Copy constructor
+        Rect(const SDL_Rect &rect)
+        {
+            x = rect.x;
+            y = rect.y;
+            w = rect.w;
+            h = rect.h;
+        }
+
+        //@}
+
+        //! @name Operators
+        //@{
+
+        //! Type-casting operator to SDL_Rect
+        operator SDL_Rect() const
+        {
+            return *this;
+        }
+        //! operator = (from SDL_Rect)
+        Rect &operator=(const SDL_Rect &rect)
+        {
+            x = rect.x;
+            y = rect.y;
+            w = rect.w;
+            h = rect.h;
+            return *this;
+        }
+
+        friend std::ostream& operator<<(std::ostream& os, const Rect& c){
+            return os << "Rect(" << c.x << ',' << c.y << ',' << c.w << ',' << c.h << ')';
+        }
+
+        //@}
+
+        //! @name Placement & Size
+        //@{
+        
+        //! Set x,y to given position
+        void setPosition(const SPoint &position)
+        {
+            x = position.x;
+            y = position.y;
+        }
+        //! Set w,h to given size
+        void setSize(const UPoint &size)
+        {
+            w = size.x;
+            h = size.y;
+        }
+        //! Return current positon
+        SPoint getPosition() const
+        {
+            return SPoint(x, y);                        
+        }
+        //! Return current size
+        UPoint getSize() const
+        {
+            return UPoint(w, h);
+        }
+
+        //@}
+       
+        //! @name Queries
+        //@{
+
+        //! Is point inside this Rect ?
+        /*!
+            @param p Point in question
+        */
+        template<typename T>
+        bool contains(const Point<T> &p) const
+        {
+            return (
+                    (x <= p.x && p.x <= x+w) &&
+                    (y <= p.y && p.y <= y+h));
+        }
+        
+        //! Is a rect inside this Rect ? (partial match required)
+        /*!
+            Whole Rect has to be inside or equal this Rect.
+            @param r Roint in question
+        */
+        bool containsWhole(const Rect &r) const
+        {
+            return (
+                    (x <= r.x && r.x+r.w <= x+w) &&
+                    (y <= r.y && r.y+r.h <= y+h));                
+        }
+        //! Is a rect inside this Rect ? (partial match is enough)
+        /*!
+            Rect in question must have at least a pixel common with this Rect.
+            @param r Rect in question
+        */
+        bool containsPartial(const Rect &r) const
+        {
+            return (
+                    (x <= r.x+r.w && r.x <= x+w) &&
+                    (y <= r.y+r.h && r.y <= y+h));                
+        }
+
+        //@}
+};
+
+//! Constant Rect
+typedef const Rect &ConstRect;
+
+#endif // DUNE_RECT_H
