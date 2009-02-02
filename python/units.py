c0623e7b11ea99f8adbadae068c54e0678c494ca
Per Øyvind Karlsen <proyvind@gmail.com>
2009-02-02 03:32:49 +0100

    start on migrating unit configuration from libconfig to python

---
+++ python/units.py
@@ -0,0 +1,187 @@
+units = {
+    'carryall' : {
+    	'armour' : 2,
+    	'health' : 100,
+    	'offset' : [8,8],
+    	'radius' : 8,
+        'speed' : 1.8,
+    	'turn_speed' : 0.125,
+    	'view_range' : 5,
+    	'weapon_range' : 0,
+    },
+    
+    'ornithopter' : {
+        'armour' : 3,
+        'damage' : 30,
+        'max_health' : 250,
+        'radius' : 6,
+        'speed' : 1.6,
+        'turn_speed' : 0.125,
+        'view_range' : 8,
+        'weapon_range' : 5,
+    },
+    
+    'frigate' : {
+    	'armour' : 10,
+    	'health' : 10000,
+        'radius' : 16,
+    	'speed' : 1.0,
+    	'view_range' : 5,
+    	'weapon_range' : 0,
+    },
+    
+    'deviator' : {
+        'armour' : 5,
+        'maxHealth' : 200,
+        'num_weapons' : 1,
+        'primary_weapon_reload_time' : 285,
+        'radius' : 6,
+        'speed' : 0.5,
+        'view_range' : 8,
+        'weapon_range' : 6,
+    },
+
+    'launcher' : {
+        'armour' : 7,
+        'health' : 300,
+        'radius' : 6,
+        'primary_weapon_reload_time' : 285,
+        'speed' : 0.2,
+    },
+    
+    'mcv' : {
+        'armour' : 7,
+        'health' : 600,
+        'radius' : 9,
+        'speed' : 0.125,
+        'view_range' : 7,
+    },
+
+    'quad' : {
+        'filename' : "DUNE:UNITS.SHP",
+        'health' : 450,
+        'num_weapons' : 2,
+        'picture_array' : 0,
+        'primary_weapon_reload_time' : 100,
+        'radius' : 6,
+        'speed' : 0.64,
+        'view_range' : 4,
+        'weapon_damage' : 25,
+        'weapon_range' : 3,
+        'size' : [1,1],
+    },
+
+    'raider' : {
+        'armour' : 3,
+        'damage' : 20,
+        'health' : 250,
+        'num_weapons' : 2,
+        'speed' : 1.3,
+        'radius' : 4,
+        'view_range' : 6,
+        'weapon_range' : 5,
+    },        
+
+    'sandworm' : {
+        'armour' : 5,
+        'damage' : 1000,
+        'health' : 3000,
+        'num_weapons' : 0,
+        'primary_weapon_reload_time' : 200,
+        'radius' : 8,
+        'speed' : 0.3,
+        'turn_speed' : 0.625,
+        'view_range' : 8,
+        'weapon_range' : 0,
+    },
+    
+    'siege_tank' : {
+        'armour' : 12,
+        'health' : 600,
+        'num_weapons' : 2,
+        'radius' : 7,
+        'speed' : 0.3,
+        'view_range' : 8,
+        'weapon_range' : 6,
+    },
+    
+    'sonic_tank' : {
+        'armour' : 7,
+        'damage' : 70,
+        'health' : 350,
+        'num_weapons' : 1,
+        'primary_weapon_reload_time' : 150,
+        'radius' : 6,
+        'speed' : 0.5,
+        'view_range' : 8,
+        'weapon_range' : 7,
+    },
+    
+    'tank' : {
+        'armour' : 10,
+        'health' : 400,
+        'radius' : 6,
+        'speed' : 0.5,
+        'turret_turn_speed' : 0.0625,
+        'view_range' : 6,
+        'weapon_range' : 5,
+    },
+    
+    'trike' : {
+        'armour' : 4,
+	'health' : 250,
+	'num_weapons' : 2,
+	'radius' : 4,
+	'speed' : 1.0.
+	'view_range' : 4,
+	'weapon_range' : 3,
+	},
+
+    'trike' : {
+        'armour' : 4,
+        'health' : 250,
+        'num_weapons' : 2,
+        'radius' : 4,
+        'speed' : 1.0,
+        'view_range' : 4,
+        'weapon_range' : 3,
+    },
+
+
+
+    'fremen' : {
+        'speed' : 0.25,
+    },
+    
+    'saboteur' : {
+        'armour' : 1,
+        'health' : 100,
+        'num_weapons' : 0,
+        'radius' : 2,
+        'speed' : 0.3,
+        'view_range' : 4,
+        'weapon_range' : 0,
+    },
+    
+    'sardaukar' : {
+        'armour' : 1,
+        'health' : 100,
+        'speed' : 0.18,        
+
+    },
+    
+    'trooper' : {
+        'armour' : 1,
+        'damage' : 50,
+        'health' : 100,
+        'num_weapons' : 1,
+        'primary_weapon_reload_time' : 300,
+        'radius' : 2,
+        'speed' : 0.2,
+        'view_range' : 4,
+        'weapon_range' : 7,
+    },
+
+},
+
+# vim:ts:8:sw:4:et
