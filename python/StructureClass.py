11362dd9d6fbbcac4507702f05f8777f11ef8c66
Per Øyvind Karlsen <proyvind@gmail.com>
2009-02-03 00:13:03 +0100

    make structure objects..

---
+++ python/StructureClass.py
@@ -0,0 +1,20 @@
+import ObjectClass
+from WeaponClass import Weapon
+
+class Structure(ObjectClass.Object):
+    def __init__(self):
+        ObjectClass.Object.__init__(self)
+	self.capacity = 0
+	self.maxHealth = 1000
+	self.powerRequirement = 0
+	self.size = (2,2)
+	self.upgradeLevels = 0
+	self.upgradeCost = 0
+        self.weapons = []
+
+class Builder(Structure):
+    def __init__(self):
+        Structure.__init__(self)
+	self.upgradeLevels = 1
+	self.upgradeCost = 500
+
--- python/structures.py
+++ python/structures.py
@@ -1,147 +1,147 @@
+from StructureClass import *
+
+class Barracks(Builder):
+    def __init__(self):
+        Builder.__init__(self)
+        self.maxHealth = 800
+        self.powerRequirement = 20
+        self.viewRange = 4
+
+class ConstructionYard(Builder):
+    def __init__(self):
+        Builder.__init__(self)
+        self.maxHealth = 1500
+        self.powerRequirement = 0
+        self.viewRange = 6
+
+class GunTurret(Structure):
+    def __init__(self):
+        Structure.__init__(self)
+        self.maxHealth = 700
+        self.powerRequirement = 10
+        self.size = (1,1)
+        self.turnSpeed = 0.01
+
+class HeavyFactory(Builder):
+    def __init__(self):
+        Builder.__init__(self)
+        self.powerRequirement = 35
+        self.size = (3,2)
+        self.upgradeLevels = 2
+
+class HighTechFactory(Builder):
+    def __init__(self):
+        Builder.__init__(self)
+        self.powerRequirement = 35
+        self.size = (3,2)
+
+
+class LightFactory(Builder):
+    def __init__(self):
+        Builder.__init__(self)
+        self.powerRequirement = 20
+
+class Starport(Builder):
+    def __init__(self):
+        Builder.__init__(self)
+        self.maxHealth = 3000
+        self.powerRequirement = 80
+        self.size = (3,3)
+        self.viewRange = 6
+
+class Wor(Builder):
+    def __init__(self):
+        Builder.__init__(self)
+        self.maxHealth = 1500
+        self.powerRequirement = 20
+
+
+class Concrete(Structure):
+    def __init__(self):
+        Structure.__init__(self)
+        self.maxHealth = 5
+        self.size = (1,1)
+
+class IX(Structure):
+    def __init__(self):
+        Structure.__init__(self)
+        self.powerRequirement = 40
+        self.viewRange = 3
+
+class Palace(Structure):
+    def __init__(self):
+        Structure.__init__(self)
+        self.maxHealth = 2000
+        self.powerRequirement = 200
+        self.viewRange = 8
+
+class Radar(Structure):
+    def __init__(self):
+        Structure.__init__(self)
+        self.powerRequirement = 20
+        self.viewRange = 8
+
+class Refinery(Structure):
+    def __init__(self):
+        Structure.__init__(self)
+        self.capacity = 2000
+        self.maxHealth = 1500
+        self.powerRequirement = 30
+        self.size = (3,2)
+        self.viewAngle = 6
+
+class RepairYard(Structure):
+    def __init__(self):
+        Structure.__init__(self)
+        self.size = (3,2)
+        self.powerRequirement = 30
+
+class RocketTurret(Structure):
+    def __init__(self):
+        Structure.__init__(self)
+        self.maxHealth = 700
+        self.powerRequirement = 15
+        self.size = (1,1)
+        self.viewRange = 7
+
+class Silo(Structure):
+    def __init__(self):
+        Structure.__init__(self)
+        self.capacity = 1000
+        self.maxHealth = 750
+        self.viewRange = 3
+    
+class Wall(Structure):
+    def __init__(self):
+        Structure.__init__(self)
+        self.maxHealth = 500
+        self.size = (1,1)
+        self.viewRange = 2
+
+class Windtrap(Structure):
+    def __init__(self):
+        Structure.__init__(self)
+        self.viewRange = 3
+
 structures = {
-    'barracks' :
-    {
-        'health' : 800,
-        'power_requirement' : 20,
-        'view_range' : 4,
-    },
+        'Barracks' : Barracks(),
+        'Const Yard' : ConstructionYard(),
+        'Light Fctry' : LightFactory(),
+        'Heavy Fctry' : HeavyFactory(),
+        'Hi-Tech' : HighTechFactory(),
+        'Star Port' : Starports(),
+        'WOR' : WOR(),
 
-    'construction_yard' : 
-    {
-        'health' : 1500,
-        'power_requirement' : 0,
-        'size' : [2,2],
-        'view_range' : 6,
-        'upgrade_levels' : 1,
-        'upgrade_cost' : 500,    
-    },
+        'Concrete' : Concrete(),
+        'IX' : IX(),
+        'Radar' : Radar(),
+        'Refinery' : Refinery(),
+        'Repair Yard' : RepairYard(),
+        'R-Turret' : RocketTurret(),
+        'Turret' : GunTurret(),
+        'Spice Silo' : Silo(),
+        'Wall' : Wall(),
+        'Windtrap' : Windtrap(),
+        }
 
-    'gun_turret' : {
-        'damage' : 100,
-        'health' : 700,
-        'power_requirement' : 10,
-        'primary_weapon_reload_time' : 100,
-        'view_range' : 5,
-        'weapon_range' : 5,
-        'size' : [1,1],
-        'turn_Speed' : 0.01,
-        'attackSound' : "Sound_gunTurret",
-        'bulletType' : "Bullet_Shell",
-    },
-    
-    'heavy_factory' : {
-        'health' : 1000,
-        'power_requirement' : 35,
-        'size' : [3,2],
-        'upgrade_levels' : 2,
-        'upgrade_cost' : 500,    
-        'view_range' : 5,        
-    },
-
-    'high_tech_factory' : {
-        'health' : 1000,
-        'power_requirement' : 35,
-        'size' : [3,2],
-        'view_range' : 5,
-    },
-
-    'ix' : {
-        'size' : [2,2],
-        'health' : 1000,
-        'power_requirement' : 40,
-        'view_range' : 3,
-    },
-    
-    'light_factory' : {
-        'size' : [2,2],
-        'health' : 1000,
-        'power_requirement' : 20,
-        'upgrade_Levels' : 1,
-        'upgrade_cost' : 500,
-        'view_range' : 5,
-    },
-    
-    'palace' : {
-        'health' : 2000,
-        'power_requirement' : 200,
-        'view_range' : 8,
-    },
-    
-    'rocket_turret' : {
-        'damage' : 100,
-        'health' : 700,
-        'power_requirement' : 15,
-        'size' : [1,1],
-        'view_range' : 7,
-        'weapon_range' : 9,
-        'attackSound' : "Sound_rocket",
-        'bulletType' : "Bullet_Rocket",
-    },
-    
-    'refinery' : {
-        'capacity' : 2000,
-        'health' : 1500,
-        'power_requirement' : 30,
-        'size' : [3,2],
-        'view_ange' : 6,
-    },
-    
-    'radar' : {
-        'health' : 1000,
-        'power_requirement' : 20,
-        'view_range' : 8,
-        'size' : [2,2],
-    },
-
-    'repair_yard' : {
-        'size' : [3,2],
-        'health' : 1000,
-        'power_requirement' : 30,
-        'view_range' : 5,
-    },
-
-    'silo' : 
-    {
-        'capacity' : 1000,
-        'health' : 750,
-        'picture_array' : 25,
-        'power_req' : 5,
-        'view_range' : 3,
-        'size' : [2,2],
-    },
-    
-    'starport' : {
-        'health' : 3000,
-        'power_requirement' : 80,
-        'size' : [3,3],
-        'view_range' : 6,
-        # Some starport specific variables
-        'price_modifier' : 1.0,
-        #'modifierTimer' : STARPORT_CHANGE_PRICETIME,
-        #define STARPORT_ARRIVETIME			5000
-        #define STARPORT_CHANGE_PRICETIME	5000
-        #define STARPORT_MIN_MOD			65
-        #define STARPORT_MAX_MOD			125
-  
-    },
-    
-    'wall' : {
-        'health' : 500,
-        'size' : [1,1],
-        'view_range' : 2,
-    },
-    
-    'windtrap' : {
-        'health' : 1000,
-        'power_requirement' : 0,
-        'size' : [2,2],
-        'view_range' : 3,
-    },
-    
-    'wor' : {
-        'health' : 1500,
-        'power_requirement' : 20,
-        'size' : [2,2],
-        'view_range' : 5,
-    },
-}
+# vim:ts=4:sw=4:et
