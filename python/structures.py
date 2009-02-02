7b6e48ec52bef93391b4c2f40d41d7f98902cbfa
Per Øyvind Karlsen <proyvind@gmail.com>
2009-02-03 00:50:59 +0100

    * add pictures * fix a typo

--- python/structures.py
+++ python/structures.py
@@ -3,6 +3,7 @@ from StructureClass import *
 class Barracks(Builder):
     def __init__(self):
         Builder.__init__(self)
+        self.picture = "ObjPic_Barracks"
         self.maxHealth = 800
         self.powerRequirement = 20
         self.viewRange = 4
@@ -10,6 +11,7 @@ class Barracks(Builder):
 class ConstructionYard(Builder):
     def __init__(self):
         Builder.__init__(self)
+        self.picture = "ObjPic_ConstructionYard"
         self.maxHealth = 1500
         self.powerRequirement = 0
         self.viewRange = 6
@@ -17,6 +19,7 @@ class ConstructionYard(Builder):
 class GunTurret(Structure):
     def __init__(self):
         Structure.__init__(self)
+        self.picture = "ObjPic_GunTurret"
         self.maxHealth = 700
         self.powerRequirement = 10
         self.size = (1,1)
@@ -25,6 +28,7 @@ class GunTurret(Structure):
 class HeavyFactory(Builder):
     def __init__(self):
         Builder.__init__(self)
+        self.picture = "ObjPic_HeavyFactory"
         self.powerRequirement = 35
         self.size = (3,2)
         self.upgradeLevels = 2
@@ -32,26 +36,29 @@ class HeavyFactory(Builder):
 class HighTechFactory(Builder):
     def __init__(self):
         Builder.__init__(self)
+        self.picture = "ObjPic_HighTechFactory"
         self.powerRequirement = 35
         self.size = (3,2)
-
 
 class LightFactory(Builder):
     def __init__(self):
         Builder.__init__(self)
+        self.picture = "ObjPic_LightFactory"
         self.powerRequirement = 20
 
 class Starport(Builder):
     def __init__(self):
         Builder.__init__(self)
+        self.picture = "ObjPic_StarPort"
         self.maxHealth = 3000
         self.powerRequirement = 80
         self.size = (3,3)
         self.viewRange = 6
 
-class Wor(Builder):
+class WOR(Builder):
     def __init__(self):
         Builder.__init__(self)
+        self.picture = "ObjPic_WOR"
         self.maxHealth = 1500
         self.powerRequirement = 20
 
@@ -65,12 +72,14 @@ class Concrete(Structure):
 class IX(Structure):
     def __init__(self):
         Structure.__init__(self)
+        self.picture = "ObjPic_IX"
         self.powerRequirement = 40
         self.viewRange = 3
 
 class Palace(Structure):
     def __init__(self):
         Structure.__init__(self)
+        self.picture = "ObjPic_Palace"        
         self.maxHealth = 2000
         self.powerRequirement = 200
         self.viewRange = 8
@@ -78,12 +87,14 @@ class Palace(Structure):
 class Radar(Structure):
     def __init__(self):
         Structure.__init__(self)
+        self.picture = "ObjPic_Radar"        
         self.powerRequirement = 20
         self.viewRange = 8
 
 class Refinery(Structure):
     def __init__(self):
         Structure.__init__(self)
+        self.picture = "ObjPic_Refinery"        
         self.capacity = 2000
         self.maxHealth = 1500
         self.powerRequirement = 30
@@ -93,12 +104,14 @@ class Refinery(Structure):
 class RepairYard(Structure):
     def __init__(self):
         Structure.__init__(self)
+        self.picture = "ObjPic_RepairYard"        
         self.size = (3,2)
         self.powerRequirement = 30
 
 class RocketTurret(Structure):
     def __init__(self):
         Structure.__init__(self)
+        self.picture = "ObjPic_RocketTurret"        
         self.maxHealth = 700
         self.powerRequirement = 15
         self.size = (1,1)
@@ -107,6 +120,7 @@ class RocketTurret(Structure):
 class Silo(Structure):
     def __init__(self):
         Structure.__init__(self)
+        self.picture = "ObjPic_Silo"        
         self.capacity = 1000
         self.maxHealth = 750
         self.viewRange = 3
@@ -114,6 +128,7 @@ class Silo(Structure):
 class Wall(Structure):
     def __init__(self):
         Structure.__init__(self)
+        self.picture = "ObjPic_Wall"        
         self.maxHealth = 500
         self.size = (1,1)
         self.viewRange = 2
@@ -121,6 +136,7 @@ class Wall(Structure):
 class Windtrap(Structure):
     def __init__(self):
         Structure.__init__(self)
+        self.picture = "ObjPic_Windtrap"        
         self.viewRange = 3
 
 structures = {
@@ -129,7 +145,7 @@ structures = {
         'Light Fctry' : LightFactory(),
         'Heavy Fctry' : HeavyFactory(),
         'Hi-Tech' : HighTechFactory(),
-        'Star Port' : Starports(),
+        'Star Port' : Starport(),
         'WOR' : WOR(),
 
         'Concrete' : Concrete(),
