e9fbffba4dfe54305ac582811e5cb0cb6a7f9ac2
Per Øyvind Karlsen <proyvind@gmail.com>
2009-02-03 23:06:07 +0100

    remove default settings which has been moved out to python object

--- src/units/InfantryClass.cpp
+++ src/units/InfantryClass.cpp
@@ -6,23 +6,6 @@
 InfantryClass::InfantryClass(PlayerClass* newOwner, std::string unitName) : GroundUnit(newOwner, unitName)
 {
 	m_attributes |= OBJECT_INFANTRY;
-	m_deathFrame = "ObjPic_DeadInfantry";
-	m_numDeathFrames = 2;
-
-	m_armour = 1;
-	m_maxHealth = 10;
-	m_health = m_maxHealth;
-	m_radius = 2;
-	m_speed = 0.25;
-	m_viewRange = 4;
-	m_weaponRange = 6;
-
-	m_numWeapons = 1;	
-	//m_bulletType[0] = Bullet_Inf_Gun;
-	m_primaryWeaponReloadTime = 55;
-	
-	w = m_pic->getSize().x / 4;
-	h = m_pic->getSize().y / 3;
 }
 
 InfantryClass::~InfantryClass()
