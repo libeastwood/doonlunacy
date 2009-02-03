e18c1fcfedec756ad122270d280fe52c48e61d58
Per Øyvind Karlsen <proyvind@gmail.com>
2009-02-03 02:04:33 +0100

    add some more default settings..

--- python/StructureClass.py
+++ python/StructureClass.py
@@ -4,7 +4,10 @@ from WeaponClass import Weapon
 class Structure(ObjectClass.Object):
     def __init__(self):
         ObjectClass.Object.__init__(self)
+	self.animate = True
 	self.capacity = 0
+	self.firstAnimFrame = 2
+	self.lastAnimFrame = 3
 	self.maxHealth = 1000
 	self.powerRequirement = 0
 	self.size = (2,2)
