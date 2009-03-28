5c8f7ea9f5937fd8dc68f9bb07185c86644f624d
Per Øyvind Karlsen <proyvind@gmail.com>
2009-03-27 22:34:29 -0400

    optimize assignToMap() further and also fix issue with object getting disappearing

--- src/objects/ObjectClass.cpp
+++ src/objects/ObjectClass.cpp
@@ -85,14 +85,10 @@ void ObjectClass::assignToMap(SPoint pos)
 {
     MapClass* map = m_owner->getMap();
 
-    for (int i = x; i < x + w/BLOCKSIZE; i++)
-	for (int j = y; j < y + h/BLOCKSIZE; j++) {
-	    SPoint temp(i,j);
-	    if (map->cellExists(temp)) {
-		map->getCell(temp)->assignObject(getObjectID());
-		map->viewMap(m_owner->getTeam(), getPosition(), m_viewRange);
-	    }
-	}
+    if (map->cellExists(pos)) {
+	map->getCell(pos)->assignObject(getObjectID());
+	map->viewMap(m_owner->getTeam(), getPosition(), m_viewRange);
+    }
 }
 
 bool ObjectClass::canAttack(ObjectClass* object)
