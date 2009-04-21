b94cae905c1ee21eac442617d131da14798d038c
Per Øyvind Karlsen <proyvind@gmail.com>
2009-04-21 16:06:24 +0200

    do a memcpy hack to make it possible to use Mix_FreeChunk() in destructor

--- src/Sfx.cpp
+++ src/Sfx.cpp
@@ -7,8 +7,16 @@ Sound::Sound(Mix_Chunk *soundChunk) : Mix_Chunk(*soundChunk) {
 }
 
 Sound::~Sound() {
-    if(allocated)
-	free(abuf);
+    /* This is a workaround for using Mix_FreeChunk as we cannot call it on
+       the object itself in it's destructor as it would attempt to free it.
+       Therefore we just copy the struct with it's members to a new variable
+       so that the same data will get freed along with this variable.
+    */
+
+    Mix_Chunk *chunk = (Mix_Chunk *)SDL_malloc(sizeof(*chunk));
+    SDL_memcpy(chunk, this, sizeof(*chunk));
+
+    Mix_FreeChunk(chunk);
 }
 
 void Sound::concatSound(SoundPtr sound) {
@@ -22,7 +30,7 @@ void Sound::concatSound(SoundPtr sound) {
 	    throw(std::bad_alloc());
 	}
 
-	memcpy(abuf, sound->abuf, sound->alen);
+	SDL_memcpy(abuf, sound->abuf, sound->alen);
     }
     else {
 	alen += sound->alen;
@@ -30,7 +38,7 @@ void Sound::concatSound(SoundPtr sound) {
 	if((abuf = (Uint8 *)realloc(abuf, alen)) == NULL)
 	    throw(std::bad_alloc());
 
-	memcpy(abuf + (alen - sound->alen), sound->abuf, sound->alen);
+	SDL_memcpy(abuf + (alen - sound->alen), sound->abuf, sound->alen);
     }
 }
 
