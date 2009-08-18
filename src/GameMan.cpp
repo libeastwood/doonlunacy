7b4281d5a30be56a09c66ffcaf370c82063ffa22
Per Øyvind Karlsen <proyvind@gmail.com>
2009-08-18 11:49:47 +0200

    indent

--- src/GameMan.cpp
+++ src/GameMan.cpp
@@ -39,20 +39,20 @@ void GameMan::AddPlayer(HOUSETYPE House, bool ai, int team)
 {
     if (m_players.size() > (unsigned)House)
     {
-        LOG(LV_ERROR, "GameMan" , "Trying to create already existing player!");
-        exit(EXIT_FAILURE);
+	LOG(LV_ERROR, "GameMan" , "Trying to create already existing player!");
+	exit(EXIT_FAILURE);
     }
 
     if (ai == true)
     {
-        //player[House] = new AiPlayerClass(House,House,House,DEFAULT_STARTINGCREDITS,InitSettings->Difficulty,team);
-        LOG(LV_WARNING, "GameMan" , "Trying to create unimplemented ai player!");
+	//player[House] = new AiPlayerClass(House,House,House,DEFAULT_STARTINGCREDITS,InitSettings->Difficulty,team);
+	LOG(LV_WARNING, "GameMan" , "Trying to create unimplemented ai player!");
     }
 
     else
     {
 	m_players.push_back(new PlayerClass(House, House, House, DEFAULT_STARTINGCREDITS, team));
-        m_localPlayer = m_players[0];//localPlayer;
+	m_localPlayer = m_players[0];//localPlayer;
     }
 
     m_players[House]->assignMapPlayerNum(House);
@@ -117,20 +117,20 @@ ObjectPtr GameMan::createObject(std::string itemName, PlayerClass* Owner)
     }
 
     if(objectClass == "AirUnit")
-	    newObject.reset(new AirUnit(Owner, itemName));
+	newObject.reset(new AirUnit(Owner, itemName));
     else if(objectClass == "GroundUnit")
-	    newObject.reset(new GroundUnit(Owner, itemName));
+	newObject.reset(new GroundUnit(Owner, itemName));
     else if(objectClass == "InfantryUnit")
-	    newObject.reset(new InfantryClass(Owner, itemName));
+	newObject.reset(new InfantryClass(Owner, itemName));
     else if(objectClass == "Object")
-	    newObject.reset(new ObjectClass(Owner, itemName));
-/*    else if(objectClass == "Unit")
-	    newObject = new UnitClass(Owner, itemName);
-	    */
+	newObject.reset(new ObjectClass(Owner, itemName));
+    /*    else if(objectClass == "Unit")
+	  newObject = new UnitClass(Owner, itemName);
+	  */
     else if(objectClass == "Builder")
-	    newObject.reset(new BuilderClass(Owner, itemName));
+	newObject.reset(new BuilderClass(Owner, itemName));
     else if(objectClass == "Structure")
-	    newObject.reset(new StructureClass(Owner, itemName));
+	newObject.reset(new StructureClass(Owner, itemName));
     else
 	LOG(LV_ERROR, "GameMan", "createObject: item %s, %s unknown type!", itemName.c_str(), objectClass.c_str());
 
@@ -141,8 +141,8 @@ ObjectPtr GameMan::createObject(std::string itemName, PlayerClass* Owner)
 
 bool GameMan::LoadScenario(string scenarioName)
 {
-	size_t len;
-	uint8_t *data = ResMan::Instance()->readFile(scenarioName, &len);
+    size_t len;
+    uint8_t *data = ResMan::Instance()->readFile(scenarioName, &len);
 
     IniFile *myInifile = new IniFile(data, len);
 
@@ -150,14 +150,14 @@ bool GameMan::LoadScenario(string scenarioName)
 
     if (SeedNum == -1)
     {
-        LOG(LV_ERROR, "GameMan", "Cannot find Seednum in %s!", scenarioName.c_str());
-        delete m_map;
-        return false;
+	LOG(LV_ERROR, "GameMan", "Cannot find Seednum in %s!", scenarioName.c_str());
+	delete m_map;
+	return false;
     }
 
     string FieldString = myInifile->getStringValue("MAP", "Field");
     string BloomString = myInifile->getStringValue("MAP", "Bloom");
-    
+
     m_map = MapGenerator::Instance()->createOldMap(FieldString, SeedNum, BloomString);
     // now set up all the players
 
@@ -174,37 +174,37 @@ bool GameMan::LoadScenario(string scenarioName)
 
     while (!myInifile->KeyList_EOF(myListHandle))
     {
-        string tmpkey = myInifile->KeyList_GetNextKey(&myListHandle);
-        string tmp = myInifile->getStringValue("UNITS", tmpkey);
-        string HouseStr, UnitStr;
+	string tmpkey = myInifile->KeyList_GetNextKey(&myListHandle);
+	string tmp = myInifile->getStringValue("UNITS", tmpkey);
+	string HouseStr, UnitStr;
 	int health, pos;
-        sScanf(tmp, "%S,%S,%d,%d", &HouseStr, &UnitStr, &health, &pos);
+	sScanf(tmp, "%S,%S,%d,%d", &HouseStr, &UnitStr, &health, &pos);
 
-        int house;
+	int house;
 
-        if ((HouseStr == "Atreides") || (HouseStr == "ATREIDES"))
-            house = HOUSE_ATREIDES;
-        else if ((HouseStr == "Ordos") || (HouseStr == "ORDOS"))
-            house = HOUSE_ORDOS;
-        else if ((HouseStr == "Harkonnen") || (HouseStr == "HARKONNEN"))
-            house = HOUSE_HARKONNEN;
-        else if ((HouseStr == "Fremen") || (HouseStr == "FREMEN"))
-            house = HOUSE_FREMEN;
-        else if ((HouseStr == "Sardaukar") || (HouseStr == "SARDAUKAR"))
-            house = HOUSE_SARDAUKAR;
-        else if ((HouseStr == "Mercenary") || (HouseStr == "MERCENARY"))
-            house = HOUSE_MERCENARY;
-        else
-        {
-            LOG(LV_WARNING, "GameMan", "LoadScenario: Invalid house string: %s", HouseStr.c_str());
-            house = HOUSE_ATREIDES;
-        }
+	if ((HouseStr == "Atreides") || (HouseStr == "ATREIDES"))
+	    house = HOUSE_ATREIDES;
+	else if ((HouseStr == "Ordos") || (HouseStr == "ORDOS"))
+	    house = HOUSE_ORDOS;
+	else if ((HouseStr == "Harkonnen") || (HouseStr == "HARKONNEN"))
+	    house = HOUSE_HARKONNEN;
+	else if ((HouseStr == "Fremen") || (HouseStr == "FREMEN"))
+	    house = HOUSE_FREMEN;
+	else if ((HouseStr == "Sardaukar") || (HouseStr == "SARDAUKAR"))
+	    house = HOUSE_SARDAUKAR;
+	else if ((HouseStr == "Mercenary") || (HouseStr == "MERCENARY"))
+	    house = HOUSE_MERCENARY;
+	else
+	{
+	    LOG(LV_WARNING, "GameMan", "LoadScenario: Invalid house string: %s", HouseStr.c_str());
+	    house = HOUSE_ATREIDES;
+	}
 
-        if (pos <= 0)
-        {
-            LOG(LV_WARNING, "GameMan", "LoadScenario: Invalid position string: %d", pos);
-            pos = 0;
-        }
+	if (pos <= 0)
+	{
+	    LOG(LV_WARNING, "GameMan", "LoadScenario: Invalid position string: %d", pos);
+	    pos = 0;
+	}
 
 	ObjectPtr newUnit = m_players[house]->placeUnit(UnitStr, UPoint(pos % 64, pos / 64));
 	if (!newUnit)
@@ -218,83 +218,83 @@ bool GameMan::LoadScenario(string scenarioName)
 
     while (!myInifile->KeyList_EOF(myListHandle))
     {
-        string tmpkey = myInifile->KeyList_GetNextKey(&myListHandle);
-        string tmp = myInifile->getStringValue("STRUCTURES", tmpkey);
+	string tmpkey = myInifile->KeyList_GetNextKey(&myListHandle);
+	string tmp = myInifile->getStringValue("STRUCTURES", tmpkey);
 
-        if (tmpkey.find("GEN") == 0)
-        {
-            // Gen Object/Structure
-            string PosStr = tmpkey.substr(3, tmpkey.size() - 3);
-            int pos = atoi(PosStr.c_str());
+	if (tmpkey.find("GEN") == 0)
+	{
+	    // Gen Object/Structure
+	    string PosStr = tmpkey.substr(3, tmpkey.size() - 3);
+	    int pos = atoi(PosStr.c_str());
 
-            string HouseStr, BuildingStr;
-            sScanf(tmp, "%S,%S", &HouseStr, &BuildingStr);
+	    string HouseStr, BuildingStr;
+	    sScanf(tmp, "%S,%S", &HouseStr, &BuildingStr);
 
-            int house;
+	    int house;
 
-            if ((HouseStr == "Atreides") || (HouseStr == "ATREIDES"))
-                house = HOUSE_ATREIDES;
-            else if ((HouseStr == "Ordos") || (HouseStr == "ORDOS"))
-                house = HOUSE_ORDOS;
-            else if ((HouseStr == "Harkonnen") || (HouseStr == "HARKONNEN"))
-                house = HOUSE_HARKONNEN;
-            else if ((HouseStr == "Fremen") || (HouseStr == "FREMEN"))
-                house = HOUSE_FREMEN;
-            else if ((HouseStr == "Sardaukar") || (HouseStr == "SARDAUKAR"))
-                house = HOUSE_SARDAUKAR;
-            else if ((HouseStr == "Mercenary") || (HouseStr == "MERCENARY"))
-                house = HOUSE_MERCENARY;
-            else
-            {
-                LOG(LV_WARNING, "GameMan", "LoadScenario: Invalid house string: %s", HouseStr.c_str());
-                house = HOUSE_ATREIDES;
-            }
+	    if ((HouseStr == "Atreides") || (HouseStr == "ATREIDES"))
+		house = HOUSE_ATREIDES;
+	    else if ((HouseStr == "Ordos") || (HouseStr == "ORDOS"))
+		house = HOUSE_ORDOS;
+	    else if ((HouseStr == "Harkonnen") || (HouseStr == "HARKONNEN"))
+		house = HOUSE_HARKONNEN;
+	    else if ((HouseStr == "Fremen") || (HouseStr == "FREMEN"))
+		house = HOUSE_FREMEN;
+	    else if ((HouseStr == "Sardaukar") || (HouseStr == "SARDAUKAR"))
+		house = HOUSE_SARDAUKAR;
+	    else if ((HouseStr == "Mercenary") || (HouseStr == "MERCENARY"))
+		house = HOUSE_MERCENARY;
+	    else
+	    {
+		LOG(LV_WARNING, "GameMan", "LoadScenario: Invalid house string: %s", HouseStr.c_str());
+		house = HOUSE_ATREIDES;
+	    }
 
-            /*
-                //FIXME: Fix this here and in addPlayer
-                if(m_players->size() > house) {
-                LOG(LV_ERROR, "MapGenerator","player[%d]== NULL",(int) house);
-                exit(EXIT_FAILURE);
-               }
-            */
+	    /*
+	    //FIXME: Fix this here and in addPlayer
+	    if(m_players->size() > house) {
+	    LOG(LV_ERROR, "MapGenerator","player[%d]== NULL",(int) house);
+	    exit(EXIT_FAILURE);
+	    }
+	    */
 
 
-            //Using INVALID_POS instead of NONE to avoid warnings.
-            //FIXME: Maybe we should rename INVALID_POS to INVALID or sth
-            if (BuildingStr == "Concrete")
-                m_players[house]->placeStructure(INVALID_POS, INVALID_POS, "Slab1", UPoint(pos % 64, pos / 64));
-            else if (BuildingStr == "Wall")
-                m_players[house]->placeStructure(INVALID_POS, INVALID_POS, "Wall", UPoint(pos % 64, pos / 64));
-            else
-                LOG(LV_WARNING, "GameMan", "LoadScenario: Invalid building string: %s", BuildingStr.c_str());
-        }
+	    //Using INVALID_POS instead of NONE to avoid warnings.
+	    //FIXME: Maybe we should rename INVALID_POS to INVALID or sth
+	    if (BuildingStr == "Concrete")
+		m_players[house]->placeStructure(INVALID_POS, INVALID_POS, "Slab1", UPoint(pos % 64, pos / 64));
+	    else if (BuildingStr == "Wall")
+		m_players[house]->placeStructure(INVALID_POS, INVALID_POS, "Wall", UPoint(pos % 64, pos / 64));
+	    else
+		LOG(LV_WARNING, "GameMan", "LoadScenario: Invalid building string: %s", BuildingStr.c_str());
+	}
 
-        else
-        {
-            // other structure
-            string HouseStr, BuildingStr;
+	else
+	{
+	    // other structure
+	    string HouseStr, BuildingStr;
 	    int health, pos;
 	    sScanf(tmp, "%S,%S,%d,%d", &HouseStr, &BuildingStr, &health, &pos);
 
-            int house;
+	    int house;
 
-            if ((HouseStr == "Atreides") || (HouseStr == "ATREIDES"))
-                house = HOUSE_ATREIDES;
-            else if ((HouseStr == "Ordos") || (HouseStr == "ORDOS"))
-                house = HOUSE_ORDOS;
-            else if ((HouseStr == "Harkonnen") || (HouseStr == "HARKONNEN"))
-                house = HOUSE_HARKONNEN;
-            else if ((HouseStr == "Fremen") || (HouseStr == "FREMEN"))
-                house = HOUSE_FREMEN;
-            else if ((HouseStr == "Sardaukar") || (HouseStr == "SARDAUKAR"))
-                house = HOUSE_SARDAUKAR;
-            else if ((HouseStr == "Mercenary") || (HouseStr == "MERCENARY"))
-                house = HOUSE_MERCENARY;
-            else
-            {
-                LOG(LV_WARNING, "GameMan", "LoadScenario: Invalid house string: %s", HouseStr.c_str());
-                house = HOUSE_ATREIDES;
-            }
+	    if ((HouseStr == "Atreides") || (HouseStr == "ATREIDES"))
+		house = HOUSE_ATREIDES;
+	    else if ((HouseStr == "Ordos") || (HouseStr == "ORDOS"))
+		house = HOUSE_ORDOS;
+	    else if ((HouseStr == "Harkonnen") || (HouseStr == "HARKONNEN"))
+		house = HOUSE_HARKONNEN;
+	    else if ((HouseStr == "Fremen") || (HouseStr == "FREMEN"))
+		house = HOUSE_FREMEN;
+	    else if ((HouseStr == "Sardaukar") || (HouseStr == "SARDAUKAR"))
+		house = HOUSE_SARDAUKAR;
+	    else if ((HouseStr == "Mercenary") || (HouseStr == "MERCENARY"))
+		house = HOUSE_MERCENARY;
+	    else
+	    {
+		LOG(LV_WARNING, "GameMan", "LoadScenario: Invalid house string: %s", HouseStr.c_str());
+		house = HOUSE_ATREIDES;
+	    }
 
 	    //Using INVALID_POS instead of NONE to avoid warnings.
 	    //FIXME: Maybe we should rename INVALID_POS to INVALID or sth
@@ -302,7 +302,7 @@ bool GameMan::LoadScenario(string scenarioName)
 
 	    if (newStructure == NULL)
 		LOG(LV_WARNING, "GameMan", "LoadScenario: Invalid position: %d", pos);
-        }
+	}
     }
 
     myInifile->KeyList_Close(&myListHandle);
@@ -310,11 +310,11 @@ bool GameMan::LoadScenario(string scenarioName)
 }
 
 /*
-void GameMan::Select(List* objectList)
-{
+   void GameMan::Select(List* objectList)
+   {
 
-}
-*/
+   }
+   */
 
 void GameMan::TakeMapScreenshot(string filename)
 {
@@ -324,8 +324,8 @@ void GameMan::TakeMapScreenshot(string filename)
 
 
     for (int i = 0 ; i < w; i++)
-        for (int j = 0 ; j < h; j++)
-            m_map->getCell(UPoint(i, j))->draw(img, SPoint(16*i, 16*j));
+	for (int j = 0 ; j < h; j++)
+	    m_map->getCell(UPoint(i, j))->draw(img, SPoint(16*i, 16*j));
 
     img->saveBMP(filename);
 
@@ -333,25 +333,25 @@ void GameMan::TakeMapScreenshot(string filename)
 }
 
 /*
-void GameMan::Unselect(List* objectList)
-{
+   void GameMan::Unselect(List* objectList)
+   {
 
-}
-*/
+   }
+   */
 void GameMan::Update(float dt)
 {
     for(ObjectTypeMap::const_iterator objTypeMap = m_objects.begin(); objTypeMap != m_objects.end(); objTypeMap++)
     {
 	ObjectMap ObjMap = (*objTypeMap).second;
 	std::stack<uint32_t> eraseStack;
-    	for(ObjectMap::const_iterator objMap = ObjMap.begin(); objMap != ObjMap.end(); objMap++)
-    	{
-    	    ObjectPtr object = objMap->second;
-    	    if (object->clearObject())
+	for(ObjectMap::const_iterator objMap = ObjMap.begin(); objMap != ObjMap.end(); objMap++)
+	{
+	    ObjectPtr object = objMap->second;
+	    if (object->clearObject())
 		eraseStack.push(objMap->first);
-    	    else
-    		object->update(dt);
-    	}
+	    else
+		object->update(dt);
+	}
 	while(!eraseStack.empty())
 	{
 	    ObjMap.erase(eraseStack.top());
